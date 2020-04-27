ddf317bfa9a1a8713fb3a64d87e8d4f0a6e7ef84
sebamateja <sebastian.matejasm@gmail.com>
2020-04-27 14:38:50 +0200

    Added speed movement object. Working on health bar

---
+++ 3D_Platformer/Assets/3DPlatformer/Scripts/HealthScript.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthScript : MonoBehaviour
+{
+    [SerializeField]
+    private int maxHealth = 100;
+
+    private int currentHealth;
+
+    public event Action<float> OnHealthPctChanged = delegate { };
+
+    private void OnEnable()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void ModifyHealth(int amount)
+    {
+        currentHealth += amount;
+
+        float currentHealthPct = (float)currentHealth / (float)maxHealth;
+
+        OnHealthPctChanged(currentHealthPct);
+    }
+}
---
+++ 3D_Platformer/Assets/3DPlatformer/Scripts/PerksOnFloor/RotatableObject.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatableObject : MonoBehaviour
+{
+    public string name1 = "Ring";
+    public string name2 = "Skill";
+
+    private float rotationSpeed = 100.0f;
+    private GameObject gameObject1;
+    private GameObject gameObject2;
+
+    void Start()
+    {
+        gameObject1 = GetChildWithName(transform, name1);
+        gameObject2 = GetChildWithName(transform, name2);
+    }
+
+    void FixedUpdate()
+    {
+        //gameObject1.transform.localRotation *= Quaternion.AngleAxis(rotationSpeed * Time.deltaTime, Vector3.right);
+        //gameObject2.transform.localRotation *= Quaternion.AngleAxis(rotationSpeed * Time.deltaTime, Vector3.right);
+        gameObject1.transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+        gameObject2.transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+    }
+
+    GameObject GetChildWithName(Transform trans, string name)
+    {
+        Transform childTrans = trans.Find(name);
+
+        if (childTrans != null)
+        {
+            Debug.Log("!= null");
+            return childTrans.gameObject;
+        } else
+        {
+            Debug.Log("== null");
+            return null;
+        }
+    }
+}
